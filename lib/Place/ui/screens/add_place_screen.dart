3c1170789323298bc12af3f91d8e4eb6d884bcda
Alex Velasco <cristianvc-99@hotmail.com>
2020-06-08 04:20:49 -0500

    Agregado widget TextField con icono Location

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:platzi_trips_app/Place/ui/widgets/title_input_location.dart';
 import 'package:platzi_trips_app/User/ui/widgets/title_header.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
 import 'package:platzi_trips_app/widgets/text_input.dart';
@@ -62,9 +63,18 @@ class _AddPlaceScreen extends State<AddPlaceScreen>{
                   margin: EdgeInsets.only(bottom: 20.0),
                   child: TextInput(hintText: "Title", inputextType: null,maxLines: 1 , controller: _controllerTitlePlace),
                 ),
-                TextInput(hintText: "Description", inputextType: TextInputType.multiline, maxLines: 4, controller: _controllerDescription)
+                TextInput(
+                    hintText: "Description",
+                    inputextType: TextInputType.multiline,
+                    maxLines: 4,
+                    controller: _controllerDescription),
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: TextInputLocation( hintText: "Add Location",iconData: Icons.location_on , controller: null),
+                ),
               ],
             ),
+
           )
         ],
       ),
